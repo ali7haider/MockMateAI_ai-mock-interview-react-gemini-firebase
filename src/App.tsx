1ef18743177b79d12eb4d03efbc1eca54c6a8791
ali7haider <ali7haider.dev@gmail.com>
2025-01-27 00:09:28 +0500

    Set up TailwindCSS and create utility functions; add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  return <div className="text-blue-500">App</div>;
+  return (
+    <div className="text-blue-500">
+      <Button>Click me</Button>;
+    </div>
+  );
 }
 
 export default App;
